1bbea0f1339188c54fadbacb1c67347686a278ae
Linda Gibson <dakidz@live.com>
2015-02-08 15:46:22 -0800

    use _POST to access the contents of contact form sent and then print out

--- contact-process.php
+++ contact-process.php
@@ -1,5 +1,10 @@
 <?php
 
-echo "I have information from a form.  Now what?";
+$name = $_POST["name"];
+$email = $_POST["email"];
+$message = $_POST["message"];
+echo $name;
+echo $email;
+echo $message;
 
 ?>
