7474627b904498c3723e7ee6059248a2c46920d2
Linda Gibson <dakidz@live.com>
2015-02-08 15:51:55 -0800

    use text to describe data in form

--- contact-process.php
+++ contact-process.php
@@ -3,8 +3,11 @@
 $name = $_POST["name"];
 $email = $_POST["email"];
 $message = $_POST["message"];
+echo "Name: ";
 echo $name;
+echo "Email: ";
 echo $email;
+echo "Message: ";
 echo $message;
 
 ?>
