884d26999c02393e423b8d934993e29cac2aadee
Linda Gibson <dakidz@live.com>
2015-02-08 16:21:29 -0800

    add header, footer and content after submitting form to contact-process page

--- contact-process.php
+++ contact-process.php
@@ -7,5 +7,26 @@ $email_body = "";
 $email_body = $email_body . "Name: " . $name . "\n";
 $email_body = $email_body . "Email: " . $email . "\n";
 $email_body = $email_body . "Message: " . $message . "\n";
-echo $email_body
+
+// comment - TODO: Send Email
+
+$pageTitle = "Contact Mike";
+$section = "contact";
+include("inc/header.php");
+
 ?>
+
+  <div class="section page">
+
+    <div class="wrapper";>
+
+      <h1>Contact</h1>
+
+      <p>Thanks for the email!  I&rsquo;ll be in touch shortly.</p>
+
+    </div>
+
+  </div>
+
+<?php include("inc/footer.php"); ?>
+
