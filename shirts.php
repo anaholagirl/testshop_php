d6edc83da450ce62ba0115e8def17d2b268f04a2
Linda Gibson <dakidz@live.com>
2015-02-08 18:25:41 -0800

    create an array of products and list themout

--- shirts.php
+++ shirts.php
@@ -1,11 +1,31 @@
 <?php
+
+$products = array(
+  "Logo Shirt, Red",
+  "Mike the Frog Shirt, Black",
+  "Mike the Frog Shirt, Blue"
+  );
+$products[] = "Log Shirt, Green";
+
+?>
+<?php
 $pageTitle = "Mike's Full Catalog of Shirts";
 $section = "shirts";
 include('inc/header.php'); ?>
 
   <div class="section page">
 
+    <div class="wrapper">
+
       <h1>Mike&rsquo;s Full Catalog of Shirts</h1>
+
+      <ul>
+        <?php foreach($products as $product) { ?>
+        <li><?php echo $product; ?></li>
+        <?php } ?>
+      </ul>
+
+    </div>
 
   </div>
 
