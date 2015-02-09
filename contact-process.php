9ca7f822457bf5d6592024bd688b550fb939c8fc
Linda Gibson <dakidz@live.com>
2015-02-08 16:32:27 -0800

    create a redirect to thanks page

--- contact-process.php
+++ contact-process.php
@@ -10,5 +10,11 @@ $email_body = $email_body . "Message: " . $message . "\n";
 
 // comment - TODO: Send Email
 
+// redirect after we send the email
+
+header("Location: contact-thanks.php");
+
+// this is useful so that if visitor uses back button on browser it won't send form twice
+
 ?>
 
