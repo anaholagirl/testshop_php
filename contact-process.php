e05ead864a85b6eeae6cbdcde75d839b9467c7cd
Linda Gibson <dakidz@live.com>
2015-02-08 16:11:52 -0800

    create variable to encompass the form information for emailing

--- contact-process.php
+++ contact-process.php
@@ -3,8 +3,9 @@
 $name = $_POST["name"];
 $email = $_POST["email"];
 $message = $_POST["message"];
-echo "Name: " . $name . "\n";
-echo "Email: " . $email . "\n";
-echo "Message: " . $message . "\n";
-
+$email_body = "";
+$email_body = $email_body . "Name: " . $name . "\n";
+$email_body = $email_body . "Email: " . $email . "\n";
+$email_body = $email_body . "Message: " . $message . "\n";
+echo $email_body
 ?>
