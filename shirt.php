135f8787ecd153260902a2c1ed0a3ac00b9b5315
Linda Gibson <dakidz@live.com>
2015-02-09 17:42:03 -0800

    specify section and pageTitle and include header and footer

--- shirt.php
+++ shirt.php
@@ -3,3 +3,11 @@
 $product_id = $_GET["id"];
 $product = $products[$product_id]; // loading particular shirt you need with the product_id
 
+$section = "shirts";
+$pageTitle = $product["name"];
+include("inc/header.php"); ?>
+
+
+
+<?php include("inc/footer.php"); ?>
+
