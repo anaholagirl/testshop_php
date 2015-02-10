6bac78f6799933a575567d7732d8c12addbcb908
Linda Gibson <dakidz@live.com>
2015-02-09 17:42:43 -0800

    start adding HTML to page

--- shirt.php
+++ shirt.php
@@ -7,7 +7,16 @@ $section = "shirts";
 $pageTitle = $product["name"];
 include("inc/header.php"); ?>
 
+  <div class="section page">
 
+    <div class="wrapper">
+
+
+
+
+    </div>
+
+  </div>
 
 <?php include("inc/footer.php"); ?>
 
