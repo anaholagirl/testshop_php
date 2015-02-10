a2a944abe811cdbb4ccbd8a7d69a82eba660a71a
Linda Gibson <dakidz@live.com>
2015-02-09 18:17:00 -0800

    add product details next to product image

--- shirt.php
+++ shirt.php
@@ -7,18 +7,32 @@ $section = "shirts";
 $pageTitle = $product["name"];
 include("inc/header.php"); ?>
 
-  <div class="section page">
+    <div class="section page">
 
-    <div class="wrapper">
+      <div class="wrapper">
 
-      <div class="breadcrumb">
-        <a href="shirts.php">Shirts</a> &gt; <?php echo $product["name"]; ?>
+        <div class="breadcrumb">
+          <a href="shirts.php">Shirts</a> &gt; <?php echo $product["name"]; ?>
+        </div>
+
+        <div class="shirt-picture">
+          <span>
+            <img src="<?php echo $product["img"]; ?>" alt="<?php echo $product["name"]; ?>">
+          </span>
+        </div>
+
+        <div class="shirt-details">
+
+          <h1><span class="price">$<?php echo $product["price"]; ?></span> <?php echo $product["name"]; ?></h1>
+
+
+          <p><em>* All shirts are designed by Mike the Frog.</em></p>
+
+        </div>
+
       </div>
 
-
     </div>
-
-  </div>
 
 <?php include("inc/footer.php"); ?>
 
