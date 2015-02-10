416a73e468386d8f6cc7e192956376503b896668
Linda Gibson <dakidz@live.com>
2015-02-09 18:19:53 -0800

    transfer form from form.php page to shirt.php page and delete form.php

--- shirt.php
+++ shirt.php
@@ -25,6 +25,25 @@ include("inc/header.php"); ?>
 
           <h1><span class="price">$<?php echo $product["price"]; ?></span> <?php echo $product["name"]; ?></h1>
 
+          <form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post">
+            <input type="hidden" name="cmd" value="_s-xclick">
+            <input type="hidden" name="hosted_button_id" value="907DLECFDRLKE">
+            <input type="hidden" name="item_name" value="Logo Shirt, Red">
+            <table>
+              <tr>
+                <th>
+                  <input type="hidden" name="on0" value="Size">
+                  <label for="os0">Size</label>
+                </th>
+              <td><select name="os0" id="os0">
+                <option value="Small">Small </option>
+                <option value="Medium">Medium </option>
+                <option value="large">Large </option>
+                <option value="X-Large">X-Large </option>
+              </select></td></tr>
+            </table>
+            <input type="submit" value="Add to Cart" name="submit">
+          </form>
 
           <p><em>* All shirts are designed by Mike the Frog.</em></p>
 
